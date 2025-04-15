a2524d5a92e506eb55c02563003dae7ee637166c
skhaldoon <skhaldoon@gmail.com>
2025-04-15 10:39:31 +0500

    Final backend

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const chatRoutes = require("./routes/chatRoutes");
-const serverless = require("serverless-http");
 
 
 dotenv.config();
@@ -24,7 +23,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
-//module.exports.handler = serverless(app);
